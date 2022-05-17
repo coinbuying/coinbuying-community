992ca8b001627128e84388942529190f991cf626
huisoo <huiwater@gmail.com>
2022-05-17 19:59:46 +0900

    eureka client 등록

--- src/main/java/coinbuying/community/CommunityApplication.java
+++ src/main/java/coinbuying/community/CommunityApplication.java
@@ -2,8 +2,11 @@ package coinbuying.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CommunityApplication {
 
 	public static void main(String[] args) {
